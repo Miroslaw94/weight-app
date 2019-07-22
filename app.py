2b87865c412fcee28c6bd709a9ca4c9802984a6c
Miroslaw94 <miras_94@wp.pl>
2019-07-22 17:25:11 +0200

    Added main functions.

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import argparse
 
+from database import Database
+import user
+
+
+Database.initialise(database='weight-app', user='postgres', password='Pass0987', host='localhost')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-n', '--name', type=str, metavar='', required=True, help="Your name")
 parser.add_argument('-w', '--weight', type=float, metavar='', help="Your today's weight")
-parser.add_argument('-g', '--graph', metavar='', help="Create your weight graph")
+parser.add_argument('-g', '--graph', action='store_true', help="Creates your weight graph")
 args = parser.parse_args()
 
 
 if __name__ == '__main__':
     print(f"Hello {args.name}! Your today's weight is {args.weight}")
+    user.check_users_table_exists()
+    if args.weight:
+        user.save_measurements(args.name, args.weight)
+        print("Weight saved.")
+    if args.graph:
+        print("Graph.")
--- user.py
+++ user.py
@@ -1,12 +1,29 @@
+from datetime import date
+
+from database import CursorFromConnectionPool
 
 
-class User:
-    def __init__(self, name, height):
-        self.name = name
-        self.height = height
+def save_measurements(name, weight):
+    check_user_exists(name)
+    with CursorFromConnectionPool() as cursor:
+        cursor.execute('INSERT INTO {} (date, weight) VALUES (%s, %s)'.format(name), (date.today(), weight,))
 
-    def __repr__(self):
-        return "{}".format(self.name)
+def check_user_exists(name):
+    with CursorFromConnectionPool() as cursor:
+        cursor.execute('SELECT EXISTS(SELECT * FROM information_schema.tables WHERE table_name=%s)', (name,))
+        user_exists = cursor.fetchone()[0]
+        if not user_exists:
+            create_new_user(name)
 
-    def save_user(self, name, height):
-        pass
+def create_new_user(name):
+    with CursorFromConnectionPool() as cursor:
+        height = input('How tall are you? (in centimeters) ')
+        cursor.execute('INSERT INTO users (name, height) VALUES (%s, %s)', (name, height))
+        cursor.execute('CREATE TABLE {} (id serial, date date, weight numeric)'.format(name))
+
+def check_users_table_exists():
+    with CursorFromConnectionPool() as cursor:
+        cursor.execute('SELECT EXISTS(SELECT * FROM information_schema.tables WHERE table_name=%s)', ('users',))
+        table_exists = cursor.fetchone()[0]
+        if not table_exists:
+            cursor.execute('CREATE TABLE public.users (id serial PRIMARY KEY, name char (255), height integer)')
